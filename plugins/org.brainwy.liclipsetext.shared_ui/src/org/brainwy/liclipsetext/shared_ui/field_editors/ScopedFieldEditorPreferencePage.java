2e38120030b1bd62adcc74d96acd93f55fafabc1
Fabio Zadrozny <fabiofz@gmail.com>
2016-05-19 09:24:54 -0300

    Porting code from PyDev to LiClipseText.

---
+++ plugins/org.brainwy.liclipsetext.shared_ui/src/org/brainwy/liclipsetext/shared_ui/field_editors/ScopedFieldEditorPreferencePage.java
@@ -0,0 +1,303 @@
+package org.brainwy.liclipsetext.shared_ui.field_editors;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.brainwy.liclipsetext.shared_core.log.Log;
+import org.brainwy.liclipsetext.shared_core.preferences.IScopedPreferences;
+import org.brainwy.liclipsetext.shared_core.string.StringUtils;
+import org.brainwy.liclipsetext.shared_core.structure.Tuple;
+import org.brainwy.liclipsetext.shared_core.utils.Reflection;
+import org.brainwy.liclipsetext.shared_ui.EditorUtils;
+import org.brainwy.liclipsetext.shared_ui.SharedUiPlugin;
+import org.brainwy.liclipsetext.shared_ui.dialogs.DialogHelpers;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Button;
+
+public abstract class ScopedFieldEditorPreferencePage extends FieldEditorPreferencePage {
+
+    private List<FieldEditor> fields;
+
+    public ScopedFieldEditorPreferencePage(int style) {
+        super(style);
+    }
+
+    public ScopedFieldEditorPreferencePage(String title, int style) {
+        super(title, style);
+    }
+
+    @Override
+    protected void addField(FieldEditor editor) {
+        super.addField(editor);
+        if (fields == null) {
+            fields = new ArrayList<FieldEditor>();
+        }
+        fields.add(editor);
+    }
+
+    public void saveToUserSettings(IScopedPreferences iScopedPreferences) {
+        Map<String, Object> saveData = getFieldEditorsSaveData();
+        if (saveData.size() > 0) {
+            try {
+                String message = iScopedPreferences.saveToUserSettings(saveData);
+                DialogHelpers.openInfo("Results", message);
+            } catch (Exception e) {
+                Log.log(e);
+                ErrorDialog.openError(EditorUtils.getShell(),
+                        "Error: unable to save requested settings to user settings",
+                        e.getMessage(),
+                        SharedUiPlugin.makeErrorStatus(e, false));
+            }
+        } else {
+            // This shouldn't happen
+            DialogHelpers.openCritical("Error: No preferences to save",
+                    "Error: No preferences to save (please report this as an error).");
+        }
+    }
+
+    public void loadFromUserSettings(IScopedPreferences iScopedPreferences) {
+        Map<String, Object> saveData = getFieldEditorsSaveData();
+        if (saveData.size() > 0) {
+            try {
+                Tuple<Map<String, Object>, Set<String>> loadedFromUserSettings = iScopedPreferences
+                        .loadFromUserSettings(saveData);
+
+                updateFieldEditorsData(loadedFromUserSettings.o1);
+
+                if (loadedFromUserSettings.o1.size() == 0) {
+                    DialogHelpers.openInfo("No saved preferences",
+                            "Unable to load any contents from the user settings.");
+
+                } else if (loadedFromUserSettings.o2.size() > 0) {
+                    DialogHelpers.openInfo("Partially loaded contents",
+                            "Partially loaded contents. Did not find the keys below in the user settings:\n  "
+                                    + StringUtils.join("\n  ", loadedFromUserSettings.o2));
+
+                } else {
+                    DialogHelpers.openInfo("Loaded contents", "Showing contents loaded from user settings.");
+
+                }
+            } catch (Exception e) {
+                Log.log(e);
+                ErrorDialog.openError(EditorUtils.getShell(),
+                        "Error: unable to load requested settings from user settings",
+                        e.getMessage(),
+                        SharedUiPlugin.makeErrorStatus(e, false));
+            }
+        } else {
+            // This shouldn't happen
+            DialogHelpers.openCritical("Error: No preferences to load",
+                    "Error: No preferences to load (please report this as an error).");
+        }
+    }
+
+    public void loadFromProjectSettings(IScopedPreferences iScopedPreferences, IProject project) {
+        Map<String, Object> saveData = getFieldEditorsSaveData();
+        if (saveData.size() > 0) {
+            try {
+                Tuple<Map<String, Object>, Set<String>> loadedFromUserSettings = iScopedPreferences
+                        .loadFromProjectSettings(saveData, project);
+
+                updateFieldEditorsData(loadedFromUserSettings.o1);
+
+                if (loadedFromUserSettings.o1.size() == 0) {
+                    DialogHelpers.openInfo("No saved preferences",
+                            "Unable to load any contents from the settings for the project: " + project.getName());
+
+                } else if (loadedFromUserSettings.o2.size() > 0) {
+                    DialogHelpers.openInfo("Partially loaded contents",
+                            "Partially loaded contents. Did not find the keys below in the settings for the project "
+                                    + project.getName() + ":\n  "
+                                    + StringUtils.join("\n  ", loadedFromUserSettings.o2));
+
+                } else {
+                    DialogHelpers.openInfo("Loaded contents", "Showing contents loaded from settings in project: "
+                            + project.getName());
+
+                }
+            } catch (Exception e) {
+                Log.log(e);
+                ErrorDialog.openError(EditorUtils.getShell(),
+                        "Error: unable to load requested settings from settings in project: " + project.getName(),
+                        e.getMessage(),
+                        SharedUiPlugin.makeErrorStatus(e, false));
+            }
+        } else {
+            // This shouldn't happen
+            DialogHelpers.openCritical("Error: No preferences to load",
+                    "Error: No preferences to load (please report this as an error).");
+        }
+    }
+
+    private void updateFieldEditorsData(Map<String, Object> loadData) throws IllegalArgumentException,
+            IllegalAccessException {
+        if (fields != null) {
+            Iterator<FieldEditor> e = fields.iterator();
+            while (e.hasNext()) {
+                FieldEditor pe = e.next();
+                if (pe instanceof BooleanFieldEditor) {
+                    BooleanFieldEditor booleanFieldEditor = (BooleanFieldEditor) pe;
+                    String preferenceName = booleanFieldEditor.getPreferenceName();
+                    Boolean value = (Boolean) loadData.get(preferenceName);
+                    if (value == null) {
+                        continue;
+                    }
+
+                    // Hack because the BooleanFieldEditor does not have a way to set the value in the view!
+                    Field field = Reflection.getAttrFromClass(BooleanFieldEditor.class, "checkBox");
+                    field.setAccessible(true);
+                    Button checkbox = (Button) field.get(booleanFieldEditor);
+                    checkbox.setSelection(value);
+
+                } else if (pe instanceof IntegerFieldEditor) { //IntegerFieldEditor is a subclass of StringFieldEditor (so, must come before)
+                    IntegerFieldEditor intFieldEditor = (IntegerFieldEditor) pe;
+                    String preferenceName = intFieldEditor.getPreferenceName();
+                    Object loaded = loadData.get(preferenceName);
+                    if (loaded == null) {
+                        continue;
+                    }
+                    if (loaded instanceof Integer) {
+                        Integer value = (Integer) loaded;
+                        intFieldEditor.setStringValue(Integer.toString(value));
+                    } else {
+                        intFieldEditor.setStringValue(loaded.toString());
+                    }
+
+                } else if (pe instanceof StringFieldEditor) { //IntegerFieldEditor is a subclass
+                    StringFieldEditor stringFieldEditor = (StringFieldEditor) pe;
+                    String preferenceName = stringFieldEditor.getPreferenceName();
+                    String value = (String) loadData.get(preferenceName);
+                    if (value == null) {
+                        continue;
+                    }
+                    stringFieldEditor.setStringValue(value);
+
+                } else if (pe instanceof ComboFieldEditor) {
+                    ComboFieldEditor comboFieldEditor = (ComboFieldEditor) pe;
+                    String preferenceName = comboFieldEditor.getPreferenceName();
+                    String value = (String) loadData.get(preferenceName);
+                    if (value == null) {
+                        continue;
+                    }
+                    comboFieldEditor.updateComboForValue(value);
+
+                } else if (pe instanceof RadioGroupFieldEditor) {
+                    RadioGroupFieldEditor radioGroupFieldEditor = (RadioGroupFieldEditor) pe;
+                    String preferenceName = radioGroupFieldEditor.getPreferenceName();
+                    String value = (String) loadData.get(preferenceName);
+                    if (value == null) {
+                        continue;
+                    }
+                    radioGroupFieldEditor.updateRadioForValue(value);
+
+                } else if (pe instanceof ScopedPreferencesFieldEditor || pe instanceof LinkFieldEditor
+                        || pe instanceof LabelFieldEditor) {
+                    // Ignore these ones
+
+                } else {
+                    Log.log("Unhandled field editor:" + pe);
+                }
+            }
+        }
+
+    }
+
+    public Map<String, Object> getFieldEditorsSaveData() {
+        Map<String, Object> saveData = new HashMap<>();
+        if (fields != null) {
+            Iterator<FieldEditor> e = fields.iterator();
+            while (e.hasNext()) {
+                FieldEditor pe = e.next();
+                if (pe instanceof BooleanFieldEditor) {
+                    BooleanFieldEditor booleanFieldEditor = (BooleanFieldEditor) pe;
+                    boolean booleanValue = booleanFieldEditor.getBooleanValue();
+                    String preferenceName = booleanFieldEditor.getPreferenceName();
+                    saveData.put(preferenceName, booleanValue);
+
+                } else if (pe instanceof IntegerFieldEditor) { //IntegerFieldEditor is a subclass of StringFieldEditor, so, must come first
+                    IntegerFieldEditor intFieldEditor = (IntegerFieldEditor) pe;
+                    String stringValue = intFieldEditor.getStringValue();
+                    String preferenceName = intFieldEditor.getPreferenceName();
+                    try {
+                        saveData.put(preferenceName, Integer.parseInt(stringValue));
+                    } catch (Exception e1) {
+                        saveData.put(preferenceName, 0);
+                    }
+
+                } else if (pe instanceof StringFieldEditor) { //IntegerFieldEditor is a subclass
+                    StringFieldEditor stringFieldEditor = (StringFieldEditor) pe;
+                    String stringValue = stringFieldEditor.getStringValue();
+                    String preferenceName = stringFieldEditor.getPreferenceName();
+                    saveData.put(preferenceName, stringValue);
+
+                } else if (pe instanceof ComboFieldEditor) {
+                    ComboFieldEditor comboFieldEditor = (ComboFieldEditor) pe;
+                    String stringValue = comboFieldEditor.getComboValue();
+                    String preferenceName = comboFieldEditor.getPreferenceName();
+                    saveData.put(preferenceName, stringValue);
+
+                } else if (pe instanceof RadioGroupFieldEditor) {
+                    RadioGroupFieldEditor radioGroupFieldEditor = (RadioGroupFieldEditor) pe;
+                    String stringValue = radioGroupFieldEditor.getRadioValue();
+                    String preferenceName = radioGroupFieldEditor.getPreferenceName();
+                    saveData.put(preferenceName, stringValue);
+
+                } else if (pe instanceof ScopedPreferencesFieldEditor || pe instanceof LinkFieldEditor
+                        || pe instanceof LabelFieldEditor) {
+                    // Ignore these ones
+
+                } else {
+                    Log.log("Unhandled field editor:" + pe);
+                }
+            }
+        }
+        return saveData;
+    }
+
+    public void saveToProjectSettings(IScopedPreferences iScopedPreferences, IProject[] projects) {
+        Map<String, Object> saveData = getFieldEditorsSaveData();
+        if (saveData.size() > 0) {
+            try {
+                String message = iScopedPreferences.saveToProjectSettings(saveData, projects);
+                DialogHelpers.openInfo("Contents saved", message);
+            } catch (Exception e) {
+                Log.log(e);
+                ErrorDialog.openError(EditorUtils.getShell(),
+                        "Error: unable to save requested settings to user settings",
+                        e.getMessage(),
+                        SharedUiPlugin.makeErrorStatus(e, false));
+            }
+        } else {
+            // This shouldn't happen
+            DialogHelpers.openCritical("Error: No preferences to save",
+                    "Error: No preferences to save (please report this as an error).");
+        }
+
+    }
+
+    public void saveToWorkspace() {
+        super.performApply();
+    }
+
+    public void loadFromWorkspace() {
+        if (fields != null) {
+            Iterator<FieldEditor> e = fields.iterator();
+            while (e.hasNext()) {
+                FieldEditor pe = e.next();
+                pe.load();
+            }
+        }
+    }
+
+}
---
+++ plugins/org.brainwy.liclipsetext.shared_ui/src/org/brainwy/liclipsetext/shared_ui/field_editors/ScopedPreferencesFieldEditor.java
@@ -0,0 +1,325 @@
+package org.brainwy.liclipsetext.shared_ui.field_editors;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.brainwy.liclipsetext.shared_core.preferences.IScopedPreferences;
+import org.brainwy.liclipsetext.shared_core.preferences.ScopedPreferences;
+import org.brainwy.liclipsetext.shared_core.structure.Tuple;
+import org.brainwy.liclipsetext.shared_ui.EditorUtils;
+import org.brainwy.liclipsetext.shared_ui.dialogs.ProjectSelectionDialog;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.viewers.IBaseLabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+
+public class ScopedPreferencesFieldEditor extends FieldEditor {
+
+    private Composite toolBar;
+    private IScopedPreferences iScopedPreferences;
+    private WeakReference<ScopedFieldEditorPreferencePage> preferencesPage;
+    private Label showingFrom;
+
+    /**
+     * @param name the name of the property
+     * @param linkText the text that'll appear to the user
+     * @param parent the parent composite
+     * @param selectionListener a listener that'll be executed when the linked text is clicked
+     */
+    public ScopedPreferencesFieldEditor(Composite parent, String pluginName,
+            ScopedFieldEditorPreferencePage preferencesPage) {
+        init("__UNUSED__", "Some text");
+        createControl(parent);
+        iScopedPreferences = ScopedPreferences.get(pluginName);
+        this.preferencesPage = new WeakReference<ScopedFieldEditorPreferencePage>(preferencesPage);
+    }
+
+    @Override
+    protected void adjustForNumColumns(int numColumns) {
+        GridData gd = (GridData) toolBar.getLayoutData();
+        gd.horizontalSpan = numColumns;
+    }
+
+    @Override
+    protected void doFillIntoGrid(Composite parent, int numColumns) {
+        toolBar = new Composite(parent, SWT.NONE);
+        toolBar.setLayout(new GridLayout(3, true));
+
+        final Button bt = getButtonControl(toolBar, "Save to ...");
+        bt.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                Menu menu = new Menu(bt);
+
+                MenuItem item1 = new MenuItem(menu, SWT.PUSH);
+                item1.setText("User settings");
+                item1.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        preferencesPage.get().saveToUserSettings(iScopedPreferences);
+                    }
+                });
+
+                MenuItem item2 = new MenuItem(menu, SWT.PUSH);
+                item2.setText("Project settings ...");
+                item2.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        ProjectSelectionDialog dialog = new ProjectSelectionDialog(EditorUtils.getShell(), null, true);
+                        dialog.labelProvider = createProjectsLabelProvider();
+                        dialog.setMessage("Choose the projects to which the preferences should be applied.\n"
+                                + createDecorationLabel());
+                        if (dialog.open() == Window.OK) {
+                            Object[] result = dialog.getResult();
+                            IProject[] projects = new IProject[result.length];
+                            for (int i = 0; i < result.length; i++) {
+                                projects[i] = (IProject) result[i];
+                            }
+                            preferencesPage.get().saveToProjectSettings(iScopedPreferences, projects);
+                        }
+                    }
+                });
+
+                MenuItem item3 = new MenuItem(menu, SWT.PUSH);
+                item3.setText("Workspace");
+                item3.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        preferencesPage.get().saveToWorkspace();
+                    }
+                });
+
+                Point loc = bt.getLocation();
+                Rectangle rect = bt.getBounds();
+
+                Point mLoc = new Point(loc.x, loc.y + rect.height);
+
+                menu.setLocation(bt.getShell().getDisplay().map(bt.getParent(), null, mLoc));
+
+                menu.setVisible(true);
+            }
+        });
+
+        final Button bt2 = getButtonControl(toolBar, "Show from ...");
+        bt2.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                Menu menu = new Menu(bt2);
+
+                MenuItem item1 = new MenuItem(menu, SWT.PUSH);
+                item1.setText("User settings");
+                item1.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        preferencesPage.get().loadFromUserSettings(iScopedPreferences);
+                        showingFrom.setText("Showing from: User Settings");
+                    }
+                });
+
+                MenuItem item2 = new MenuItem(menu, SWT.PUSH);
+                item2.setText("Project settings ...");
+                item2.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        ProjectSelectionDialog dialog = new ProjectSelectionDialog(EditorUtils.getShell(), null, false);
+                        dialog.labelProvider = createProjectsLabelProvider();
+                        dialog.setMessage("Choose the project from which the preferences should be shown.\n"
+                                + createDecorationLabel());
+                        if (dialog.open() == Window.OK) {
+                            IProject project = (IProject) dialog.getFirstResult();
+                            preferencesPage.get().loadFromProjectSettings(iScopedPreferences, project);
+                            showingFrom.setText("Showing from: " + project.getName());
+                        }
+                    }
+                });
+
+                MenuItem item3 = new MenuItem(menu, SWT.PUSH);
+                item3.setText("Workspace");
+                item3.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        preferencesPage.get().loadFromWorkspace();
+                        showingFrom.setText("Showing from: Workspace");
+                    }
+                });
+
+                Point loc = bt2.getLocation();
+                Rectangle rect = bt2.getBounds();
+
+                Point mLoc = new Point(loc.x, loc.y + rect.height);
+
+                menu.setLocation(bt2.getShell().getDisplay().map(bt2.getParent(), null, mLoc));
+
+                menu.setVisible(true);
+            }
+        });
+
+        final Button bt3 = getButtonControl(toolBar, "Open location ...");
+        bt3.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                Menu menu = new Menu(bt3);
+
+                MenuItem item1 = new MenuItem(menu, SWT.PUSH);
+                final File userSettingsLocation = iScopedPreferences.getUserSettingsLocation();
+                item1.setText("User settings: " + userSettingsLocation);
+                item1.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        EditorUtils.openFile(userSettingsLocation);
+                    }
+                });
+
+                MenuItem item2 = new MenuItem(menu, SWT.PUSH);
+                item2.setText("Project settings ...");
+                item2.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        ProjectSelectionDialog dialog = new ProjectSelectionDialog(EditorUtils.getShell(), null, true);
+                        dialog.labelProvider = createProjectsLabelProvider();
+                        dialog.setMessage("Choose the projects from which the preference files should be opened.\n"
+                                + createDecorationLabel());
+                        if (dialog.open() == Window.OK) {
+                            for (Object o : dialog.getResult()) {
+                                IProject p = (IProject) o;
+                                IFile projectSettingsLocation = iScopedPreferences.getProjectSettingsLocation(p);
+                                EditorUtils.openFile(projectSettingsLocation);
+                            }
+                        }
+                    }
+
+                });
+
+                Point loc = bt3.getLocation();
+                Rectangle rect = bt3.getBounds();
+
+                Point mLoc = new Point(loc.x, loc.y + rect.height);
+
+                menu.setLocation(bt3.getShell().getDisplay().map(bt3.getParent(), null, mLoc));
+
+                menu.setVisible(true);
+            }
+        });
+
+        GridData gd = createFillGridData();
+        gd.horizontalSpan = numColumns;
+        toolBar.setLayoutData(gd);
+
+        showingFrom = new Label(parent, SWT.NONE);
+        showingFrom.setText("Showing from: Workspace");
+
+    }
+
+    private GridData createFillGridData() {
+        GridData gd = new GridData();
+        gd.horizontalAlignment = GridData.FILL;
+        gd.grabExcessHorizontalSpace = true;
+        return gd;
+    }
+
+    private String createDecorationLabel() {
+        return "Legend:\n  *   project has all settings in this page\n  +   project has some settings in this page\n  ?  project has settings with errors";
+    }
+
+    private IBaseLabelProvider createProjectsLabelProvider() {
+        return new WorkbenchLabelProvider() {
+
+            private Map<Object, String> elementToDecorationCache = new HashMap<Object, String>();
+
+            @Override
+            protected String decorateText(String input, Object element) {
+                String ret = super.decorateText(input, element);
+                String decoration = getDecoration(element);
+                ret += (" " + decoration);
+                return ret;
+            }
+
+            private String getDecoration(Object element) {
+                String ret = elementToDecorationCache.get(element);
+                if (ret != null) {
+                    return ret;
+                }
+                ScopedFieldEditorPreferencePage preferencePage = preferencesPage.get();
+                Map<String, Object> saveData = preferencePage.getFieldEditorsSaveData();
+
+                String decoration;
+                try {
+                    Tuple<Map<String, Object>, Set<String>> loadFromProjectSettings = iScopedPreferences
+                            .loadFromProjectSettings(saveData, (IProject) element);
+                    if (loadFromProjectSettings.o1.size() == 0) {
+                        decoration = "";
+                    } else {
+                        if (loadFromProjectSettings.o2.size() == 0) {
+                            decoration = "*";
+                        } else {
+                            decoration = "+";
+                        }
+                    }
+                } catch (Exception e) {
+                    decoration = "?";
+                }
+                elementToDecorationCache.put(element, ret);
+                return decoration;
+            }
+        };
+    }
+
+    /**
+     * Returns this field editor's link component.
+     * <p>
+     * The link is created if it does not already exist
+     * </p>
+     *
+     * @param parent the parent
+     * @return the label control
+     */
+    private Button getButtonControl(Composite parent, String text) {
+        Button button = new Button(parent, SWT.PUSH);
+        button.setText(text);
+        button.setFont(parent.getFont());
+
+        GridData gd = createFillGridData();
+        button.setLayoutData(gd);
+        return button;
+    }
+
+    @Override
+    protected void doLoad() {
+    }
+
+    @Override
+    protected void doLoadDefault() {
+    }
+
+    @Override
+    protected void doStore() {
+    }
+
+    @Override
+    public int getNumberOfControls() {
+        return 1;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled, Composite parent) {
+        //super.setEnabled(enabled, parent); -- don't call super!
+        toolBar.setEnabled(enabled);
+    }
+}
