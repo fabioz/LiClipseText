251774e75248dcdd76bdbc768881032e6427d639
Fabio Zadrozny <fabiofz@gmail.com>
2017-06-14 16:06:55 -0300

    Updating settings for code formatting and updated test so that it does not output to stdout.

--- plugins/org.brainwy.liclipsetext.editor/tests/org/brainwy/liclipsetext/editor/tm4eintegration/Tm4eIntegrationTest.java
+++ plugins/org.brainwy.liclipsetext.editor/tests/org/brainwy/liclipsetext/editor/tm4eintegration/Tm4eIntegrationTest.java
@@ -1,5 +1,8 @@
 package org.brainwy.liclipsetext.editor.tm4eintegration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.brainwy.liclipsetext.editor.common.partitioning.TestUtils;
 import org.brainwy.liclipsetext.editor.languages.IStreamProvider;
 import org.brainwy.liclipsetext.editor.languages.LanguagesManager;
@@ -10,7 +13,7 @@ import org.eclipse.tm4e.core.registry.Registry;
 
 import junit.framework.TestCase;
 
-public class Tm4eIntegrationTest extends TestCase{
+public class Tm4eIntegrationTest extends TestCase {
 
     @Override
     protected void setUp() throws Exception {
@@ -24,23 +27,29 @@ public class Tm4eIntegrationTest extends TestCase{
         TestUtils.clearLanguagesManager();
     }
 
-	public void testTM4eIntegration() throws Exception {
-		Registry registry = new Registry();
+    public void testTM4eIntegration() throws Exception {
+        Registry registry = new Registry();
 
         LiClipseLanguage liclipseLanguage = new LanguagesManager(TestUtils.getTestLanguagesDir())
                 .getLanguageFromName("source.ts");
         IStreamProvider streamProvider = liclipseLanguage.file.getStreamProvider();
 
-		IGrammar grammar = registry.loadGrammarFromPathSync(".tmLanguage",
-				streamProvider.getStream());
+        IGrammar grammar = registry.loadGrammarFromPathSync(".tmLanguage", streamProvider.getStream());
 
-		ITokenizeLineResult result = grammar.tokenizeLine("/**");
-		for (int i = 0; i < result.getTokens().length; i++) {
-			System.err.println(result.getTokens()[i]);
-		}
-		result = grammar.tokenizeLine("**/ ", result.getRuleStack());
-		for (int i = 0; i < result.getTokens().length; i++) {
-			System.err.println(result.getTokens()[i]);
-		}
-	}
+        List<String> lst = new ArrayList<>();
+        ITokenizeLineResult result = grammar.tokenizeLine("/**");
+        for (int i = 0; i < result.getTokens().length; i++) {
+            lst.add(result.getTokens()[i].toString());
+        }
+        result = grammar.tokenizeLine("**/ ", result.getRuleStack());
+        for (int i = 0; i < result.getTokens().length; i++) {
+            lst.add(result.getTokens()[i].toString());
+        }
+
+        assertEquals(TestUtils.listToExpected(
+                "{startIndex: 0, endIndex: 3, scopes: [source.ts, comment.block.documentation.ts]}",
+                "{startIndex: 0, endIndex: 1, scopes: [source.ts, comment.block.documentation.ts]}",
+                "{startIndex: 1, endIndex: 3, scopes: [source.ts, comment.block.documentation.ts]}",
+                "{startIndex: 3, endIndex: 5, scopes: [source.ts]}"), TestUtils.listToExpected(lst));
+    }
 }
