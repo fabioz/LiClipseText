11ef17e5a00fe1bd96764ed34cd39de0286b222a
Fabio Zadrozny <fabiofz@gmail.com>
2019-10-11 07:51:28 -0300

    Compile with Eclipse 4.13 (2019-09).

--- plugins/org.brainwy.liclipsetext.shared_core/src/org/brainwy/liclipsetext/shared_core/resource_stubs/AbstractIProjectStub.java
+++ plugins/org.brainwy.liclipsetext.shared_core/src/org/brainwy/liclipsetext/shared_core/resource_stubs/AbstractIProjectStub.java
@@ -18,7 +18,6 @@ import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IPluginDescriptor;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.content.IContentTypeMatcher;
 
@@ -77,11 +76,6 @@ public class AbstractIProjectStub extends AbstractIContainerStub implements IPro
 
     @Override
     public IProjectNature getNature(String natureId) throws CoreException {
-        throw new RuntimeException("Not implemented");
-    }
-
-    @Override
-    public IPath getPluginWorkingLocation(IPluginDescriptor plugin) {
         throw new RuntimeException("Not implemented");
     }
 
@@ -192,9 +186,9 @@ public class AbstractIProjectStub extends AbstractIContainerStub implements IPro
     public int getType() {
         return IResource.PROJECT;
     }
-    
+
     public void clearCachedDynamicReferences() {
-    	
+
     }
 
 }
